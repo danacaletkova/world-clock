3e68ddba079161ea1733b53c8db2a90ca0042df2
danacaletkova <164472411+danacaletkova@users.noreply.github.com>
2024-04-09 13:35:01 +0800

    Add functionality of select

--- src/script.js
+++ src/script.js
@@ -1,23 +1,45 @@
-function updateTime() {
-  //Perth
-  let perth = document.querySelector("#perth");
-  let perthDate = perth.querySelector(".date");
-  let perthTime = perth.querySelector(".time");
+// function updateTime() {
+//   //Perth
+//   let perth = document.querySelector("#perth");
+//   let perthDate = perth.querySelector(".date");
+//   let perthTime = perth.querySelector(".time");
 
-  perthDate.innerHTML = moment().tz("Australia/Perth").format("Do MMMM yyyy");
-  perthTime.innerHTML = moment()
-    .tz("Australia/Perth")
-    .format("h:mm:ss [<small>] A [</small>]");
+//   perthDate.innerHTML = moment().tz("Australia/Perth").format("Do MMMM yyyy");
+//   perthTime.innerHTML = moment()
+//     .tz("Australia/Perth")
+//     .format("h:mm:ss [<small>] A [</small>]");
 
-  //Brno
-  let brno = document.querySelector("#brno");
-  let brnoDate = brno.querySelector(".date");
-  let brnoTime = brno.querySelector(".time");
+//   //Brno
+//   let brno = document.querySelector("#brno");
+//   let brnoDate = brno.querySelector(".date");
+//   let brnoTime = brno.querySelector(".time");
 
-  brnoDate.innerHTML = moment().tz("Europe/Prague").format("Do MMMM yyyy");
-  brnoTime.innerHTML = moment()
-    .tz("Europe/Prague")
-    .format("h:mm:ss [<small>] A [</small>]");
+//   brnoDate.innerHTML = moment().tz("Europe/Prague").format("Do MMMM yyyy");
+//   brnoTime.innerHTML = moment()
+//     .tz("Europe/Prague")
+//     .format("h:mm:ss [<small>] A [</small>]");
+// }
+
+// updateTime();
+// setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  console.log(event.target.value);
+  let cityTime = moment().tz(cityTimeZone);
+  let displayedCitiesElement = document.querySelector("#displayed-cities");
+  displayedCitiesElement.innerHTML = `          
+    <div class="city">
+      <div class="left">
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("Do MMMM yyyy")}</div>
+      </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )} <small>${cityTime.format(" A ")}</small></div>
+    </div>`;
 }
 
-setInterval(updateTime, 1000);
+let citySelectElement = document.querySelector("#select-cities");
+citySelectElement.addEventListener("change", updateCity);
